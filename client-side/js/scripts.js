dc93a825d828a5a6fe925fbfb2cd679ea7ca6981
Vladimir_Fedotov <vmfedotov@technoserv.com>
2019-05-29 17:58:35 +0300

    second commit

--- client-side/js/scripts.js
+++ client-side/js/scripts.js
@@ -59,7 +59,114 @@ function populateWithMembers() {
     }
 }
 
-$(function() {
+function getFilteredArray(namesWithMottos, strToSearch) {
+
+    var result = [];
+
+    for (i in namesWithMottos) {
+
+        if (namesWithMottos.names.startsWith(strToSearch)) {
+            result.push(namesWithMottos[i]);
+        }
+
+    }
+
+    return result;
+}
+
+function searchGlobalContacts() {
+
+    var namesWithMottos =
+        [{
+            "name": "Alexander",
+            "motto": 25
+        }, {
+            "name": "Anthony",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }, {
+            "name": "Jay",
+            "motto": 25
+        }]
+
+
+        {
+        "names": ["Alexander", "Anthony", "Andrew", "Aaron", "Asher", "Austin", "Grayson", "Gavin", "Greyson", "George", "Giovanni", "Grant"],
+        "mottos": ["Fire!", "Hey now!", "Live!", "GD", "ich bin da", "kasabian", "milk shaker", "bronson", "rhcp fan", "jhony the first", "L&M smoker", "software prog"]
+    }
+
+    if (namesWithMottos.length !== 0) {
+
+        document.getElementById("search-results").style.display = "block";
+
+        for (i in contactsName) {
+
+            var divListRow = createListRowForSearchResults(contactsName[i], contactsMotos[i]);
+
+            document.getElementById("search-results").appendChild(divListRow);
+        }
+    }
+
+}
+
+function createListRowForSearchResults(name, moto) {
+
+    var divListRow = document.createElement('div');
+    divListRow.className = "list-row";
+
+    var divContactImage = document.createElement('div');
+    divContactImage.className = "contact-image";
+
+    var divSearchCI = document.createElement('div');
+    divSearchCI.style.minWidth = "calc(100% - 100px)";
+    divSearchCI.className = "contact-info";
+
+    var divName = document.createElement('div');
+    divName.style.fontSize = "18px";
+    divName.innerHTML = name;
+
+    var divMoto = document.createElement('div');
+    divMoto.style.fontSize = "14px";
+    divMoto.innerHTML = moto;
+
+    var divAddBtnWrapper = document.createElement('div');
+    divAddBtnWrapper.className = "add-btn-wrapper";
+
+    var buttonAddBtn = document.createElement('button');
+    buttonAddBtn.className = "add-btn";
+
+    divListRow.appendChild(divContactImage);
+
+    divSearchCI.appendChild(divName);
+    divSearchCI.appendChild(divMoto);
+
+    divListRow.appendChild(divSearchCI);
+
+    divAddBtnWrapper.appendChild(buttonAddBtn);
+    divListRow.appendChild(divAddBtnWrapper);
+
+    return divListRow;
+}
+
+
+/*$(function() {
 
     //The passed argument has to be at least a empty object or a object with your desired options
     $("#user-list").overlayScrollbars({
@@ -69,6 +176,6 @@ $(function() {
     $("#conversation-list").overlayScrollbars({
         className: "os-theme-light"
     });
-});
+});*/
 
 
